1eec022d358a7b5cf993b3aef1671048491a982e
hs <hs@hs-PC>
2016-03-03 10:54:29 +0900

    login , join

--- fofo/src/org/fofo/member/dao/MemberDAOImpl.java
+++ fofo/src/org/fofo/member/dao/MemberDAOImpl.java
@@ -3,8 +3,19 @@ package org.fofo.member.dao;
 import java.util.List;
 
 import org.fofo.member.vo.Member;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.fofo.common.JDBCUtil;
 
 public class MemberDAOImpl implements MemberDAO{
+	
+	private static String doJoinSQL = "INSERT INTO user(uName, uEmail, uPW)"
+			+ "VALUES(?, ?, ?);";
+	private static String doLoginSQL = "SELECT uPW FROM user WHERE uEmail = ?";
 
 	public MemberDAOImpl() {
 		// TODO Auto-generated constructor stub
@@ -13,12 +24,60 @@ public class MemberDAOImpl implements MemberDAO{
 	@Override
 	public void doJoin(Member vo) throws Exception {
 		// TODO Auto-generated method stub
+		Connection conn = null;
+		PreparedStatement stmt = null;
 		
+		try{
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(doJoinSQL);
+			stmt.setString(1, vo.getUName());
+			stmt.setString(2, vo.getUEmail());
+			stmt.setString(3, vo.getUPW());
+			
+			int cnt = stmt.executeUpdate();	
+			if(cnt == 1){
+				System.out.println("joining had successed");
+			}else{
+				System.out.println("joining had failed");
+			}
+			
+		}catch(SQLException e){
+			System.out.println("joining occured an ERROR");
+		}finally{
+			JDBCUtil.close(stmt, conn);
+		}
+
 	}
 
 	@Override
 	public Member doLogin(Member vo) throws Exception {
 		// TODO Auto-generated method stub
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try{
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(doLoginSQL);
+			stmt.setString(1, vo.getUEmail());
+			String pw=vo.getUPW();
+			ResultSet cnt = stmt.executeQuery();	
+			if(cnt.next()){
+				if(cnt.getString("uPW")!=null&&cnt.getString("uPW").equals(pw)){
+					//일치하면
+					
+				}else{
+					
+				}
+				System.out.println("Login had successed");
+			}else{
+				System.out.println("Login had failed");
+			}
+			
+		}catch(SQLException e){
+			System.out.println("Login occured an ERROR");
+		}finally{
+			JDBCUtil.close(stmt, conn);
+		}
 		return null;
 	}
 
