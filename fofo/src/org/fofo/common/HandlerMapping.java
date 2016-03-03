24f3c7b894d2489d8dab361751799eb40f953b1c
KaylaKwon <papermoon703@gmail.com>
2016-03-03 10:43:54 +0900

    someone inserted my part _kwon

--- fofo/src/org/fofo/common/HandlerMapping.java
+++ fofo/src/org/fofo/common/HandlerMapping.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.fofo.board.controller.GetBoardController;
 import org.fofo.board.controller.GetBoardListController;
 import org.fofo.project.controller.AddProjectController;
+import org.fofo.project.controller.ProjectController;
 
 
 public class HandlerMapping {
@@ -19,14 +20,10 @@ public class HandlerMapping {
 //		mappings.put("/hello.do", new HelloController());
 		mappings.put("/login.do", new GetBoardListController());
 		mappings.put("/freeboard.do", new GetBoardListController());
-<<<<<<< HEAD
-		mappings.put("/addProjectList.do", new AddProjectController());
+//		mappings.put("/addProjectList.do", new AddProjectController());
 		mappings.put("/project.do", new ProjectController());
-=======
 		mappings.put("/getfreeboard.do", new GetBoardController());
-		mappings.put("/addProjectList.do", new AddProjectController());
-		mappings.put("/project.do", new AddProjectController());
->>>>>>> ffe1df134bc822e4580f2aae56901e9b0291f1a8
+//		mappings.put("/addProjectList.do", new AddProjectController());
 		
 	}
 	
