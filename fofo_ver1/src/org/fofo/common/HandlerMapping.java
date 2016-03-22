4729db7e8942cfc537e4e9e5bbc802adfd15dbb8
AJY <Ahnplan92@gmail.com>
2016-03-22 12:02:46 +0900

    qaboard

--- fofo_ver1/src/org/fofo/common/HandlerMapping.java
+++ fofo_ver1/src/org/fofo/common/HandlerMapping.java
@@ -18,7 +18,9 @@ import org.fofo.member.controller.DoLoginController;
 import org.fofo.member.controller.DoLogoutController;
 import org.fofo.project.controller.AddProjectController;
 import org.fofo.project.controller.GetProjectEditorController;
-
+import org.fofo.qaboard.controller.AddQABoardController;
+import org.fofo.qaboard.controller.GetQABoardListController;
+import org.fofo.qaboard.controller.GetQABoardController;
 
 public class HandlerMapping {
 
@@ -40,6 +42,9 @@ public class HandlerMapping {
 		mappings.put("/modifyfreeboard.do", new ModifyBoardController());
 		mappings.put("/removefreeboard.do", new RemoveBoardController());
 		mappings.put("/modifyviewfreeboard.do", new ModifyViewBoardController());
+		mappings.put("/qaboard.do", new GetQABoardListController());
+		mappings.put("/addqaboard.do", new AddQABoardController());
+		mappings.put("/getqaboard.do", new GetQABoardController());
 		
 		mappings.put("/addfreecomment.do", new AddCommentController());
 		mappings.put("/removefreecomment.do", new RemoveCommentController());
